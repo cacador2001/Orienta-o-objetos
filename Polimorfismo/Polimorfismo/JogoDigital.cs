33b6ba70926b2c246915a326b1c1d9cb501aa0c3
cacador2001 <84744340+cacador2001@users.noreply.github.com>
2022-04-13 09:36:31 -0300

    Add files via upload

---
+++ Polimorfismo/Polimorfismo/JogoDigital.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class JogoDigital : Produto
+    {
+        string desenvolvedora;
+        string titulo;
+        string console;
+
+        public string Desenvolvedora
+        {
+            get
+            {
+                return desenvolvedora;
+            }
+        }
+
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+        }
+
+        public string Console
+        {
+            get
+            {
+                return console;
+            }
+        }
+
+        public JogoDigital(string desenvolvedora, string titulo, string console, float preco) :base(preco)
+        {
+            this.desenvolvedora = desenvolvedora;
+            this.titulo = titulo;
+            this.console = console;
+        }
+
+        public override string InformarDescricao()
+        {
+            return $"{titulo} feito por {desenvolvedora}";
+        }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Livro : Produto
+    {
+        string desenvolvedora;
+        string titulo;
+
+        public string Desenvolvedora
+        {
+            get
+            {
+                return desenvolvedora;
+            }
+        }
+
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+        }
+
+        public Livro(string desenvolvedora, string titulo, float preco): base(preco)
+        {
+            this.desenvolvedora = desenvolvedora;
+            this.titulo = titulo;
+        }
+
+        public override string InformarDescricao()
+        {
+            return $"{titulo} escrito por {desenvolvedora}";
+        }
+    }
+}
