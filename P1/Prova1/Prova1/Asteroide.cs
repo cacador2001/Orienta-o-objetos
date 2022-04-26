e7a8ce3e105c250bdfc43b5a4381fd018ce86a6c
cacador2001 <84744340+cacador2001@users.noreply.github.com>
2022-04-26 09:16:53 -0300

    Add files via upload

---
+++ P1/Prova1/Prova1/Asteroide.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova1
+{
+    class Asteroide
+    {
+        int energia;
+        int velocidade;
+        int posicaoX;
+        int posicaoY;
+
+        public int Energia
+        {
+            get
+            {
+                return energia;
+            }
+
+            set
+            {
+                energia = value;
+            }
+        }
+
+        public int Velocidade
+        {
+            get
+            {
+                return velocidade;
+            }
+
+            set
+            {
+                velocidade = value;
+            }
+        }
+
+        public int PosicaoX
+        {
+            get
+            {
+                return posicaoX;
+            }
+
+            set
+            {
+                posicaoX = value;
+            }
+        }
+
+        public int PosicaoY
+        {
+            get
+            {
+                return posicaoY;
+            }
+
+            set
+            {
+                posicaoY = value;
+            }
+        }
+
+        public Asteroide(int energia, int velocidade, int posicaoX, int posicaoY)
+        {
+            this.energia = energia;
+            this.velocidade = velocidade;
+            this.posicaoX = posicaoX;
+            this.posicaoY = posicaoY;
+        }
+    }
+}
