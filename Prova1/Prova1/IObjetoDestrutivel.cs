0bae256b15a6790aac3504a2da638e0a4af3caef
cacador2001 <84744340+cacador2001@users.noreply.github.com>
2022-04-27 08:55:28 -0300

    Add files via upload

---
+++ Prova1/Prova1/IObjetoDestrutivel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova1
+{
+    interface IObjetoDestrutivel
+    {
+        void DanoGrave();
+        void DanoMedio();
+        void DanoLeve();
+
+    }
+}
